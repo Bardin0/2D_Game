e3803a827b7d413b9e4a7386de215ca6e2c08120
Michael Giovannini <154377009+Bardin0@users.noreply.github.com.>
2024-12-25 01:12:07 -0500

    Added collision detection

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -24,6 +24,8 @@ public class Player extends Entity{
         screenX = gp.screenWidth/2 - (gp.tileSize/2);
         screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
+        solidArea = new Rectangle(8,16,32,32);
+
         setDefaultValues();
         getPlayerImage();
 
@@ -88,19 +90,37 @@ public class Player extends Entity{
         if (keyH.downPressed || keyH.leftPressed || keyH.upPressed || keyH.rightPressed) {
             if (keyH.upPressed){
                 direction = "up";
-                worldY -= speed;
             }
             else if (keyH.downPressed){
                 direction = "down";
-                worldY += speed;
             }
             else if (keyH.leftPressed){
                 direction = "left";
-                worldX -= speed;
             }
             else if (keyH.rightPressed) {
                 direction = "right";
-                worldX += speed;
+            }
+
+            // CHECK TILE COLLISION
+            collisionOn = false;
+            gp.checker.checkTile(this);
+
+            // IF COLLISION IS FALSE, PLAYER CAN MOVE
+            if (!collisionOn){
+                switch (direction){
+                    case "up":
+                        worldY -= speed;
+                        break;
+                    case "down":
+                        worldY += speed;
+                        break;
+                    case "left":
+                        worldX -= speed;
+                        break;
+                    case "right":
+                        worldX += speed;
+                        break;
+                }
             }
 
             spriteCounter ++;
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -29,6 +29,7 @@ public class GamePanel extends JPanel implements Runnable{
     TileManager tileM = new TileManager(this);
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+    public CollisionChecker checker = new CollisionChecker(this);
     public Player player = new Player(this, keyHandler);
 
     public GamePanel(){
